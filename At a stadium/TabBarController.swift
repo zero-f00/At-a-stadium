20aef896c038ce633a5f76d2a67bbfe1eb844341
yuuto.masamura <yuto.jl8nml@gmail.com>
2020-05-03 22:37:40 +0900

    Podfile、Podfile.lockのコミット

---
+++ At a stadium/TabBarController.swift
@@ -0,0 +1,42 @@
+//
+//  TabBarController.swift
+//  At a stadium
+//
+//  Created by Yuto Masamura on 2020/05/03.
+//  Copyright © 2020 Yuto Masamura. All rights reserved.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController, UITabBarControllerDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // タブアイコンの色
+        self.tabBar.tintColor = UIColor(red: 1.0, green: 0.44, blue: 0.11, alpha: 1)
+        
+        // タブバーの背景画像
+        self.tabBar.barTintColor = UIColor(red: 255, green: 255, blue: 255, alpha: 1)
+        
+        // UITabBarControllerDelegateプロトコルのメソッドをこのクラスで処理する。
+        self.delegate = self
+    }
+    
+    // タブバーのアイコンがタップされた時に呼ばれるdelegateメソッドを処理する。
+    func tabBarController(_ tabBarController: UITabBarController, shouldSelect viewController: UIViewController) -> Bool {
+        return true
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Podfile
@@ -0,0 +1,16 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '13.1'
+
+target 'At a stadium' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for At a stadium
+  pod 'Firebase','6.11.0'
+  pod 'Firebase/Analytics'
+  pod 'Firebase/Auth'
+  pod 'Firebase/Firestore'
+  pod 'Firebase/Storage'
+  pod 'FirebaseUI/Storage'
+
+end
